50b714725c084dc1c1f96e593da24cce02c921d9
alaynapuck28 <you@example.com>
2019-07-15 20:56:08 -0500

    part of part one

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+var http = require("http");
+var state = require("./state.js").users;
+let server = http.createServer(messageReceived);
 
+server.listen(8080);
+
+function messageReceived(req, res) {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  if (req.method === "GET" && req.url === "/users") {
+    let usersJSON = JSON.stringify(state);
+    res.write(usersJSON);
+  }
+  if (req.method === "GET" && req.url === "/users/1") {
+    let user = state.find(user => user["_id"] === 1);
+    res.write(JSON.stringify(user));
+  } else {
+    res.write("your mom");
+  }
+  res.end();
+}
